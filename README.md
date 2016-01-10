de1d8897007cbae34d5f5a642985251b8244705d
Justin Watson <jwatson5@gmail.com>
2016-01-09 16:17:06 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# File Metadata Microservice
+
+## User Stories (Requirements)
+* User Story: I can submit a FormData object that includes a file upload.
+* User Story: When I submit something, I will receive the file size in bytes within the JSON response
+
+## Example
+Go to [/upload](/upload).
---
+++ app.js
@@ -0,0 +1,84 @@
+var express = require('express');
+var logger = require('morgan');
+var path = require('path');
+var router = express.Router();
+var RSVP = require("rsvp")
+var Promise = RSVP.Promise
+var fs = require("fs")
+var marked = require("marked")
+var multer  = require('multer')
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./uploads")
+    },
+    filename: (req, file, cb) => {
+        console.log(file)
+        cb(null, file.fieldname + "-" + Date.now())
+    }
+})
+var upload = multer({storage: storage}).single("userFile")
+
+var app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+
+// Routes
+////////////////////////////////////////////////////////////////////////////////
+
+router.get("/", function (req, res, next) {
+    // Render the README file.
+    var p = new Promise(function (res, rej) {
+        fs.readFile("README.md", "utf8", function (err, data) {
+            if (err) throw err
+            res(data)
+        })
+    }).then(function (val) {
+        res.status(200).send(marked(val.toString()))
+    })
+})
+
+router.get("/upload", (req, res, next) => {
+    res.sendFile(__dirname + '/public/uploadForm.html');
+})
+
+router.post("/api/upload", (req, res, next) => {
+    upload(req, res, (err) => {
+        if (err) {
+            return res.end("Error uploading file.")
+        }
+        // req.file is the "userFile"
+        console.log("got file")
+        console.log(req.file)
+        var fileStats = fs.statSync(req.file.path)
+        res.status(200).json({fileSize: fileStats.size})
+    })
+})
+
+app.use(router)
+
+// Error Handlers
+////////////////////////////////////////////////////////////////////////////////
+
+// Development Error Handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// Production Error Handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+module.exports = app;
